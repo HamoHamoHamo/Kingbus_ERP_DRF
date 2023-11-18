397fc350c7d4bcab6116c47171c77264144e5ece
HamoHamoHamo <hamo222@naver.com>
2023-11-18 21:27:41 +0900

    feat(crudmember): 거래처 목록 추가
    crudmember 앱 추가
    거래처 목록 api ClientList 추가

---
+++ crudmember/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from humanresource.models import Member
+
+class Client(models.Model):
+    business_num = models.CharField(max_length=100, verbose_name='사업자번호', null=False, blank=True)
+    name = models.CharField(max_length=100, verbose_name='거래처이름', null=False)
+    representative = models.CharField(max_length=100, verbose_name='대표자명', null=False, blank=True)
+    phone = models.CharField(max_length=100, verbose_name='대표전화', null=False)
+    manager = models.CharField(max_length=100, verbose_name='담당자', null=False, blank=True)
+    manager_phone = models.CharField(max_length=100, verbose_name='담당자번호', null=False, blank=True)
+    email = models.CharField(max_length=100, verbose_name='이메일', null=False, blank=True)
+    address = models.CharField(max_length=100, verbose_name='주소', null=False, blank=True)
+    note = models.CharField(max_length=100, verbose_name='비고', null=False, blank=True)
+
+    creator = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="client_user", null=True)
+    pub_date = models.DateTimeField(verbose_name='작성시간', auto_now_add=True, null=False)
