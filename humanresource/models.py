4fb6d65379a33bfb15829c0b850690596311fe8b
HamoHamoHamo <hamo222@naver.com>
2023-11-29 21:33:41 +0900

    feat(salary): 급여명세서, 급여확인 기능 추가

--- humanresource/models.py
+++ humanresource/models.py
@@ -37,8 +37,66 @@ class Member(models.Model):
     
     pub_date = models.DateTimeField(verbose_name="등록날짜", auto_now_add=True, null=False)
     creator = models.CharField(verbose_name='작성자 이름', max_length=100, null=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
     token = models.CharField(verbose_name='fcmtoken', max_length=500, null=False, blank=True)
     authority = models.IntegerField(verbose_name='권한', null=False, default=4)
     use = models.CharField(verbose_name='사용여부', max_length=30, null=False, default='사용')
     def __str__(self):
         return self.name
+
+class Salary(models.Model):
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="salary", null=True)
+    base = models.CharField(verbose_name='기본급', max_length=20, null=False, default=0)
+    service_allowance = models.CharField(verbose_name='근속수당', max_length=20, null=False, default=0)
+    performance_allowance = models.CharField(verbose_name='성과급', max_length=20, null=False, default=0)
+    annual_allowance = models.CharField(verbose_name='연차수당', max_length=20, null=False, default=0)
+    meal = models.CharField(verbose_name='식대', max_length=20, null=False, default=0)
+    attendance = models.CharField(verbose_name='출근요금', max_length=20, null=False)
+    leave = models.CharField(verbose_name='퇴근요금', max_length=20, null=False)
+    order = models.CharField(verbose_name='일반주문요금', max_length=20, null=False)
+    additional = models.CharField(verbose_name='추가요금', max_length=20, null=False, default=0)
+    deduction = models.CharField(verbose_name='공제', max_length=20, null=False, default=0)
+    total = models.CharField(verbose_name='총금액', max_length=20, null=False)
+    month = models.CharField(verbose_name='지급월', null=False, max_length=7)
+    payment_date = models.CharField(verbose_name='급여지급일', null=False, max_length=10, blank=True)
+    creator = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="salary_user", db_column="user_id", null=True)
+    pub_date = models.DateTimeField(verbose_name='작성시간', auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
+    
+    def __str__(self):
+        if self.member_id:
+            return self.member_id.name + ' ' + self.month
+
+class AdditionalSalary(models.Model):
+    salary_id = models.ForeignKey(Salary, on_delete=models.CASCADE, related_name="additional_salary", null=False)
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="additional_member", null=False)
+    price = models.CharField(verbose_name='금액', max_length=40, null=False, default='0')
+    remark = models.CharField(verbose_name='비고', null=False, blank=True, max_length=100)
+    creator = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="additional_user", db_column="user_id", null=True)
+    pub_date = models.DateTimeField(verbose_name='작성시간', auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
+
+    def __str__(self):
+        if self.member_id:
+            return self.member_id.name + ' ' + self.salary_id.month
+
+class DeductionSalary(models.Model):
+    salary_id = models.ForeignKey(Salary, on_delete=models.CASCADE, related_name="deduction_salary", null=False)
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="deduction_member", null=False)
+    price = models.CharField(verbose_name='금액', max_length=40, null=False, default='0')
+    remark = models.CharField(verbose_name='비고', null=False, blank=True, max_length=100)
+    creator = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="deduction_user", null=True)
+    pub_date = models.DateTimeField(verbose_name='작성시간', auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
+     
+    def __str__(self):
+        if self.member_id:
+            return self.member_id.name + ' ' + self.salary_id.month
+
+
+class SalaryChecked(models.Model):
+    salary = models.OneToOneField(Salary, on_delete=models.SET_NULL, related_name="salary_checked", null=True)
+    creator = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="salary_checked_user", null=True)
+    pub_date = models.DateTimeField(verbose_name='작성시간', auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
+    
--- humanresource/views.py
+++ humanresource/views.py
@@ -1,6 +1,7 @@
-from django import dispatch
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from django.db.models import Q
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -8,14 +9,21 @@ from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from my_settings import MAINTENANCE
 from trp_drf.pagination import Pagination
+from trp_drf.settings import DATE_FORMAT
+from django.http import Http404, HttpResponse, HttpResponseNotAllowed
 
 from .serializers import UserLoginSerializer, MemberListSerializer, MemberSerializer
-from .models import Member
+from .models import Member, Salary, SalaryChecked
+from dispatch.models import DispatchRegularlyConnect, DispatchOrderConnect
+
+WEEK = ['(월)', '(화)', '(수)', '(목)', '(금)', '(토)', '(일)', ]
+TODAY = str(datetime.now())[:10]
 
 class UserLoginView(APIView):
     serializer_class = UserLoginSerializer
@@ -161,8 +169,183 @@ class LoginMemberView(APIView):
             'message' : '',
         }
         return Response(response, status=status.HTTP_200_OK)
-    
-     
+
+#https://stackoverflow.com/questions/55416471/how-to-resolve-assertionerror-accepted-renderer-not-set-on-response-in-django
+# api_view, renderer_classes 넣어야 됨 위 링크 참고
+@api_view(('GET', 'POST'))
+#@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
+def salary_detail(request):
+    member_id = request.user.id
+    month = request.GET.get('date', TODAY[:7])
+
+    if request.method == 'POST':
+        try:
+            salary = Salary.objects.filter(member_id=member_id).get(month=month)
+        except Exception as e:
+            response = {
+                'result' : 'false',
+                'data' : '1',
+                'message' : {
+                    'error' : f"{e}"
+                },
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            salary_checked = salary.salary_checked
+            salary_checked.save()
+        except Exception as e:
+            salary_checked = SalaryChecked(salary = salary, creator = request.user)
+            salary_checked.save()
+
+        response = {
+            'result' : 'true',
+            'data' : {
+                'creator' : member_id,
+                'updated_at' : str(salary_checked.updated_at),
+                'date' : salary.month
+            },
+            'message' : '',
+        }
+        return Response(response, status=status.HTTP_200_OK)
+
+    if request.method == "GET":
+        try:
+            datetime.strptime(month+'-01', DATE_FORMAT)
+        except ValueError:
+            response = {
+                'result' : 'false',
+                'data' : 1,
+                'message' : {
+                    'error' : 'Invalid month',
+                },
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+        # try:
+        #     category_date = Category.objects.get(type='급여지급일').category
+        #     if category_date == '말일':
+        #         salary_date = datetime.strftime(datetime.strptime(month+'-01', DATE_FORMAT) + relativedelta(months=1) - timedelta(days=1), DATE_FORMAT)
+        #     else:
+        #         salary_date = f'{month}-{category_date}'
+        # except Category.DoesNotExist:
+        #     salary_date = ''
+        
+        member_list = []
+        
+        member = get_object_or_404(Member, id=member_id)
+        last_date = datetime.strftime(datetime.strptime(month+'-01', DATE_FORMAT) + relativedelta(months=1) - timedelta(days=1), DATE_FORMAT)[8:10]
+
+        attendance_list = [''] * int(last_date)
+        leave_list = [''] * int(last_date)
+        order_list = [''] * int(last_date)
+        order_price_list = [0] * int(last_date)
+        attendance_price_list = [0] * int(last_date)
+        leave_price_list = [0] * int(last_date)
+        week_list = []
+
+        order_cnt = 0
+        attendance_cnt = 0
+        leave_cnt = 0
+        
+        total_list = [0] * int(last_date)
+        work_cnt = 0
+        
+
+        salary = Salary.objects.filter(member_id=member).get(month=month)
+        meal = salary.meal
+        payment_date = salary.payment_date
+        if payment_date == '말일':
+            salary_date = datetime.strftime(datetime.strptime(month+'-01', DATE_FORMAT) + relativedelta(months=1) - timedelta(days=1), DATE_FORMAT)
+        else:
+            salary_date = f'{month}-{payment_date}'
+
+        additional = salary.additional_salary.all()
+        deduction = salary.deduction_salary.all()
+
+        connects = DispatchOrderConnect.objects.filter(departure_date__range=(f'{month}-01 00:00', f'{month}-{last_date} 24:00')).filter(driver_id=member)
+        order_cnt = connects.count()
+        for connect in connects:
+            c_date = int(connect.departure_date[8:10]) - 1
+            if not order_list[c_date]:
+                order_list[c_date] = []
+            order_list[c_date].append([connect.order_id.departure, connect.order_id.arrival])
+            
+        # if connects:
+            if connect.payment_method == 'n':
+                order_price_list[c_date] += int(connect.driver_allowance)
+                total_list[c_date] += int(connect.driver_allowance)
+
+        attendances = DispatchRegularlyConnect.objects.filter(departure_date__range=(f'{month}-01 00:00', f'{month}-{last_date} 24:00')).filter(work_type='출근').filter(driver_id=member)
+        attendance_cnt = attendances.count()
+        for attendance in attendances:            
+            c_date = int(attendance.departure_date[8:10]) - 1
+            if not attendance_list[c_date]:
+                attendance_list[c_date] = []
+            attendance_list[c_date].append([attendance.regularly_id.departure, attendance.regularly_id.arrival])
+
+            attendance_price_list[c_date] += int(attendance.driver_allowance)
+        # if attendances:
+            total_list[c_date] += int(attendance.driver_allowance)
+
+        leaves = DispatchRegularlyConnect.objects.filter(departure_date__range=(f'{month}-01 00:00', f'{month}-{last_date} 24:00')).filter(work_type='퇴근').filter(driver_id=member)
+        leave_cnt = leaves.count()
+        for leave in leaves:
+            c_date = int(leave.departure_date[8:10]) - 1
+            if not leave_list[c_date]:
+                leave_list[c_date] = []
+            leave_list[c_date].append([leave.regularly_id.departure, leave.regularly_id.arrival])
+        
+            leave_price_list[c_date] += int(leave.driver_allowance)
+        # if leaves:
+            total_list[c_date] += int(leave.driver_allowance)
+
+
+        for i in range(int(last_date)):
+            check = 0
+
+            if i + 1 < 10:
+                date = f'{month}-0{i+1}'
+            else:
+                date = f'{month}-{i+1}'
+
+            week_list.append(WEEK[datetime.strptime(date, DATE_FORMAT).weekday()])
+
+            if check == 1:
+                work_cnt += 1
+
+        total_cnt = leave_cnt + attendance_cnt + order_cnt
+        member_list.append({
+            'order_list': order_list,
+            'attendance_list': attendance_list,
+            'leave_list': leave_list,
+            'order_cnt': order_cnt,
+            'total_cnt': total_cnt,
+            'attendance_cnt': attendance_cnt,
+            'leave_cnt': leave_cnt,
+            'order_price_list': order_price_list,
+            'attendance_price_list': attendance_price_list,
+            'leave_price_list': leave_price_list,
+            'salary': salary,
+            'member': member,
+            'week_list': week_list,
+            'total_list': total_list,
+            'work_cnt': work_cnt,
+            'additional': additional,
+            'deduction': deduction,
+            'meal': meal,
+            'salary_date': salary_date,
+        })
+            
+        context = {
+            'member_list': member_list,
+            'month': month
+        }
+        return render(request, 'salary_detail.html', context)
+
+    else:
+        return HttpResponseNotAllowed(['post', 'get'])
+
 class TokenRefreshView(jwt_views.TokenRefreshView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
