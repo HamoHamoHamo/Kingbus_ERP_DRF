6b60ededf0ac5723fed1ac19da96d16da05615c4
HamoHamoHamo <hamo222@naver.com>
2024-01-24 12:42:15 +0900

    fix: 일일점검, 주간점검, 비품점검 dispatch에서 vehicle로 이동

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
-from .models import Refueling
+from .models import Refueling, DailyChecklist, WeeklyChecklist, EquipmentChecklist
 
 
 class RefuelingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Refueling
 		fields = '__all__'
+
+class DailyChecklistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyChecklist
+        fields = "__all__"
+
+
+class WeeklyChecklistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeeklyChecklist
+        fields = "__all__"
+
+
+class EquipmentChecklistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EquipmentChecklist
+        fields = "__all__"
--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.VehicleListView.as_view()),
     path('/refueling', views.RefuelingView.as_view()),
+    path('/checklist/daily/<str:date>', views.DailyChecklistView.as_view()),
+    path('/checklist/weekly/<str:date>', views.WeeklyChecklistView.as_view()),
+    path('/checklist/equipment/<str:date>', views.EquipmentChecklistView.as_view()),
 ]
