721357a7ba3e2a1f4d1861e806f161811a197c71
webdevs-lee <jsweb1008@gmail.com>
2024-12-23 21:02:56 +0900

    feat : gas fueling record list

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -24,3 +24,12 @@ class EquipmentChecklistSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipmentChecklist
         fields = "__all__"
+
+class RefuelingListSerializer(serializers.ModelSerializer) :
+    vehicle_num = serializers.CharField(source = 'vehicle.vehicle_num') # vehicle 차량 번호
+    driver_name = serializers.CharField(source = 'driver.name') # 기사 이름
+    gas_station_name = serializers.CharField(source = 'gas_station.category') # 주유소 이름
+
+    class Meta:
+         model = Refueling
+         fields = ['refueling_date', 'driver_name', 'vehicle_num', 'km', 'refueling_amount', 'urea_solution', 'gas_station_name']
